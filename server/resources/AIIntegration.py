1cb7af8447cb32d04e7ee957a1937a50b3cb3566
Ky Huynh <hgky95@gmail.com>
2024-05-20 23:08:17 -0400

    remove hardcode instead use data from request body

--- server/resources/AIIntegration.py
+++ server/resources/AIIntegration.py
@@ -1,21 +1,27 @@
 import autogen
+from flask import request, jsonify
 from autogen.agentchat.contrib.multimodal_conversable_agent import MultimodalConversableAgent
 from autogen.agentchat.contrib.web_surfer import WebSurferAgent
 
 from flask_restful import Resource
 
 
+
 class AIIntegration(Resource):
 
     def post(self):
-        img_url = "https://ichef.bbci.co.uk/news/1024/cpsprodpb/1567B/production/_133257678_gettyimages-2150435928.jpg.webp"
+        data = request.get_json()
+        article_url = data['article_url']
+        images_url = data['images_url']
 
         config_list_3 = autogen.config_list_from_dotenv(
             dotenv_file_path="../.env",
             model_api_key_map={"gpt-3.5-turbo": "OPENAI_API_KEY"},
+            #model_api_key_map={"gpt-4o": "OPENAI_API_KEY"},
             filter_dict={
                 "model": {
                     "gpt-3.5-turbo",
+                    #"gpt-4o",
                 }
             }
         )
@@ -42,10 +48,12 @@ class AIIntegration(Resource):
 
         config_list_4 = autogen.config_list_from_dotenv(
             dotenv_file_path="../.env",
-            model_api_key_map={"gpt-4-turbo": "OPENAI_API_KEY"},
+            #model_api_key_map={"gpt-4-turbo": "OPENAI_API_KEY"},
+            model_api_key_map={"gpt-4o": "OPENAI_API_KEY"},
             filter_dict={
                 "model": {
-                    "gpt-4-turbo",
+                    #"gpt-4-turbo",
+                    "gpt-4o",
                 }
             }
         )
@@ -71,14 +79,14 @@ class AIIntegration(Resource):
 
         message = f"""
            You have the context of the image from web_surfer.
-           Now, you need to answer: what is this picture of and describe everything in it based on the given context? <img {img_url}>
+           Now, you need to answer: what is this picture of and describe everything in it based on the given context? <img {images_url[0]['url']}> <img {images_url[1]['url']}>
        """
 
         chat_result = user_proxy.initiate_chats(
             [
                 {
                     "recipient": web_surfer,
-                    "message": "Summarize the content of this website 'https://www.bbc.com/news/world-us-canada-68937775'",
+                    "message": f"Summarize the content of this website {article_url}",
                     "silent": False,
                 },
                 {
