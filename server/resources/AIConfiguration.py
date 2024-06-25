821b4c57582f3f92388be5896a4e16aca43b18bc
Ky Huynh <hgky95@gmail.com>
2024-06-25 17:22:27 -0400

    update the configuration to use ollama

--- server/resources/AIConfiguration.py
+++ server/resources/AIConfiguration.py
@@ -1,47 +1,41 @@
 import autogen
+import os
 
 
 class AIConfiguration:
-    config_list_3 = autogen.config_list_from_dotenv(
-        dotenv_file_path="../.env",
-        model_api_key_map={"gpt-3.5-turbo": "OPENAI_API_KEY"},
-        # model_api_key_map={"gpt-4o": "OPENAI_API_KEY"},
-        filter_dict={
-            "model": {
-                "gpt-3.5-turbo",
-                # "gpt-4o",
-            }
+
+    config_list_llama3 = [
+        {
+            "model": "llama3",
+            "base_url": os.getenv('BASE_URL'),
+            "api_key": "ollama",
         }
-    )
+    ]
 
     web_surfer_llm_config = {
         "timeout": 600,
         "cache_seed": 11,
-        "config_list": config_list_3,
+        "config_list": config_list_llama3,
         "temperature": 0,
     }
 
     summarizer_llm_config = {
         "timeout": 600,
         "cache_seed": 33,
-        "config_list": config_list_3,
+        "config_list": config_list_llama3,
         "temperature": 0,
     }
 
-    config_list_4 = autogen.config_list_from_dotenv(
-        dotenv_file_path="../.env",
-        # model_api_key_map={"gpt-4-turbo": "OPENAI_API_KEY"},
-        model_api_key_map={"gpt-4o": "OPENAI_API_KEY"},
-        filter_dict={
-            "model": {
-                # "gpt-4-turbo",
-                "gpt-4o",
-            }
+    config_list_llava = [
+        {
+            "model": "llava",
+            "base_url": os.getenv('BASE_URL'),
+            "api_key": "ollama",
         }
-    )
+    ]
 
     image_llm_config = {
-        "config_list": config_list_4,
+        "config_list": config_list_llava,
         "temperature": 0.5,
         "max_tokens": 500,
         "cache_seed": 42
