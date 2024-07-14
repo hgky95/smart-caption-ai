40dfd34bd57db6fa40327e3201a7aeed119ea49e
Ky Huynh <hgky95@gmail.com>
2024-07-13 21:20:02 -0400

    upgrade new version to support gpt-4o

--- server/resources/AIConfiguration.py
+++ server/resources/AIConfiguration.py
@@ -4,12 +4,12 @@ import autogen
 class AIConfiguration:
     config_list_3 = autogen.config_list_from_dotenv(
         dotenv_file_path="../.env",
-        model_api_key_map={"gpt-3.5-turbo": "OPENAI_API_KEY"},
-        # model_api_key_map={"gpt-4o": "OPENAI_API_KEY"},
+        # model_api_key_map={"gpt-3.5-turbo": "OPENAI_API_KEY"},
+        model_api_key_map={"gpt-4o": "OPENAI_API_KEY"},
         filter_dict={
             "model": {
-                "gpt-3.5-turbo",
-                # "gpt-4o",
+                # "gpt-3.5-turbo",
+                "gpt-4o",
             }
         }
     )
@@ -30,11 +30,11 @@ class AIConfiguration:
 
     config_list_4 = autogen.config_list_from_dotenv(
         dotenv_file_path="../.env",
-        # model_api_key_map={"gpt-4-turbo": "OPENAI_API_KEY"},
+        #model_api_key_map={"gpt-4-turbo": "OPENAI_API_KEY"},
         model_api_key_map={"gpt-4o": "OPENAI_API_KEY"},
         filter_dict={
             "model": {
-                # "gpt-4-turbo",
+                #"gpt-4-turbo",
                 "gpt-4o",
             }
         }
